3b025c6ea15d955cbaec1d7ea9fa01d770d69d4a
Abdul Majid <majid.jutt236@gmail.com>
2024-06-07 15:49:57 +0500

    galery updated

--- src/components/gallety/Gallery.js
+++ src/components/gallety/Gallery.js
@@ -12,24 +12,28 @@ const Gallery = () => {
     <div>
       <section className="container-fluid">
         <h6 className="text-center pt-5 tast">TASTY & CRUNCHY</h6>
-        <h2 className=" text-center gal">Our Gallery</h2>
+        <h2 className="text-center gal">Our Gallery</h2>
         <p className="insp text-center">
           Inspired by recipes and creations of world’s best chefs
         </p>
         <div className="container">
-          <div className="pcs">
-            <img src={img12} alt="" className="img-fluid g1 mt-2" />
-            <img src={img13} alt="" className="img-fluid ms-2 mt-2 g2" />
-            <img src={img14} alt="" className="img-fluid ms-2 g3 mt-2" />
+          <div className="d-flex justify-content-center">
+            <div className="pcs">
+              <img src={img12} alt="" className="img-fluid g1 mt-2" />
+              <img src={img13} alt="" className="img-fluid ms-2 mt-2 g2" />
+              <img src={img14} alt="" className="img-fluid ms-2 g3 mt-2" />
+            </div>
           </div>
-          <div className="pcs">
-            <img src={img15} alt="" className="img-fluid  mt-2 g2" />
-            <img src={img16} alt="" className="img-fluid ms-2 g1 mt-2" />
-            <img src={img17} alt="" className="img-fluid ms-2 g3 mt-2" />
+          <div className="d-flex justify-content-center">
+            <div className="pcs">
+              <img src={img15} alt="" className="img-fluid mt-2 g2" />
+              <img src={img16} alt="" className="img-fluid ms-2 g1 mt-2" />
+              <img src={img17} alt="" className="img-fluid ms-2 g3 mt-2" />
+            </div>
           </div>
-          <br></br>
-          <br></br>
-          <br></br>
+          <br />
+          <br />
+          <br />
         </div>
       </section>
     </div>
